8fcaf587525476ba4a1d5bbf88f67c1c04f5f022
frtrriann <frtrriann@outlook.com>
2023-04-25 20:21:52 +0300

    fix even.js text

--- src/even.js
+++ src/even.js
@@ -3,7 +3,7 @@ import getRandomNumber from './lib.js';
 
 const isEven = (num) => num % 2 === 0;
 
-export default () => play('Answer "yes" if number even otherwise answer "no".', () => {
+export default () => play('Answer "yes" if the number is even, otherwise answer "no".', () => {
   const question = getRandomNumber(1, 100);
   const answer = isEven(question) ? 'yes' : 'no';
   return { question, answer };
